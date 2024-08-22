d17486acdc614bdb21fd702ee15b0382ae6bced8
Tom Edwards <edwardstj1@cardiff.ac.uk>
2024-08-22 09:57:10 +0100

    gui changes

--- plugins/cogni-sketch-contrib-example/functions/exampleFunction.js
+++ plugins/cogni-sketch-contrib-example/functions/exampleFunction.js
@@ -11,29 +11,57 @@ import {
 } from "/javascripts/interface/data.js";
 import {switchToPane} from "/javascripts/interface/ui.js";
 import {httpGet} from "/javascripts/interface/http.js";
+import {getPalette} from "/javascripts/private/state.js";
 import {showToast} from "/javascripts/interface/log.js";
+import {getPosFromNode} from "/javascripts/interface/graphics.js";
+import {computeNewNodePosFrom} from "/javascripts/private/util/coords.js";
+import {createNewEmptyNode} from "/javascripts/private/core/create.js";
+import {putText} from "/javascripts/interface/graphics.js"
 
 const TYPE_NAME = 'exampleFunction';
 const URL_EXAMPLE = '/example/';
 
 registerNodeExecuteCallback(TYPE_NAME, runExample);
 
-function runExample(context) {
-    if (context.node.getTypeName() === 'text') {
-        let labelText = context.node.getLabel();
-        let textProperty = context.node.getPropertyNamed('text');
+//async function runExample(context) {
+//    if (context.node.getTypeName() === 'text') {
+//        let labelText = context.node.getLabel();
+//        let textProperty = context.node.getPropertyNamed('text');
+//        let pos = getPosFromNode(context.node.getPos(),9,10);
+//        let data = await getNerForText(textProperty);
 
-        getNerForText(textProperty)
 
-        let url = URL_EXAMPLE + getProject().getName() + '?label=' + labelText + '&text=' + textProperty;
 
-        setSessionValue('exampleNode', context.node);
-        httpGet(url, callbackRunExample);
-    } else {
-        showToast('Needs to be dropped on a text node');
-    }
+//        callbackRunExample(pos,data)
+//        //setSessionValue('exampleNode', data);
+
+//    } else {
+//        showToast('Needs to be dropped on a text node');
+//    }
+//}
+
+async function runExample(context) {
+    let textProperty = context.node.getPropertyNamed('text');
+    let pos = getPosFromNode(context.node.getPos(),9,10);
+    await fetch("http://127.0.0.1:5000/ner", {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({'page': textProperty})
+    }).then(response => response.text())
+        .then((dataStr) => {
+                let data = JSON.parse(dataStr);
+                return data.output;
+            }
+        ).then(result => {
+            let strresult = JSON.stringify(result);
+            let nodeType = getPalette().getItemById('text');
+            console.log(strresult)
+            createNewEmptyNode(nodeType, strresult, pos)
+            })
 }
-
 function getNerForText(text) {
     fetch("http://127.0.0.1:5000/ner", {
         method: 'POST',
@@ -54,12 +82,14 @@ function getNerForText(text) {
 
 
 
-function callbackRunExample(res) {
-    if (res.error) {
-        showToast(`Something went wrong: ${res.error}`);
-    } else {
-        switchToPane('example')
-        setSessionValue('exampleNode', res);
-        showToast(`Processing successful`);
-    }
+function callbackRunExample(pos,res) {
+
+    //let pos = getPosFromMousePos();
+    let strresult = JSON.stringify(res);
+    let nodeType = getPalette().getItemById('text');
+    createNewEmptyNode(nodeType, strresult, pos)
+
+    //setSessionValue('exampleNode', res);
+
 }
+
